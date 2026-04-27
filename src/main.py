3f5538cd5e607d45f6c70259c4e824ef985e0a08
Juarez-J-Monteiro <juarezjrdossantos2014@gmail.com>
2026-04-27 20:45:26 -0300

    feat: Add the statistics button

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from src.ui.interface_menu import Menu
 from src.ui.interface_partida import Partida
+from src.ui.interface_estatisticas import Estatisticas
 
 root = tk.Tk()
 root.geometry("800x600")
@@ -13,13 +14,22 @@ def ir_para_jogo():
     menu.pack_forget()
     partida.pack(fill="both", expand=True)
 
+def ir_para_estatisticas():
+    menu.pack_forget()
+    estatisticas.pack(fill="both", expand=True)
+
 def voltar_menu():
     partida.pack_forget()
     menu.pack(fill="both", expand=True)
 
+def voltar_menu_estatisticas():
+    estatisticas.pack_forget()
+    menu.pack(fill="both", expand=True)
+
 # cria as telas
-menu = Menu(root, ir_para_jogo)
+menu = Menu(root, ir_para_jogo, ir_para_estatisticas)
 partida = Partida(root, voltar_menu)
+estatisticas = Estatisticas(root, voltar_menu_estatisticas)
 
 # começa pelo menu
 menu.pack(fill="both", expand=True)
