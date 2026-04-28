9619590623cab1f62cbfcd8a8629e4ec4a2b7f1f
Juarez-J-Monteiro <juarezjrdossantos2014@gmail.com>
2026-04-28 14:18:24 -0300

    style: Change the colors of the text and buttons

--- src/ui/interface_menu.py
+++ src/ui/interface_menu.py
@@ -2,15 +2,15 @@ import tkinter as tk
 
 class Menu(tk.Frame):
     def __init__(self, master, ir_para_jogo, ir_para_estatisticas):
-        super().__init__(master, bg="white")
+        super().__init__(master, bg="black")
 
-        tk.Label(self, text="DETONATOR", font=("Arial Black", 40)).pack(pady=40)
+        tk.Label(self, text="DETONATOR", font=("Arial Black", 40), fg="white", bg="black").pack(pady=40)
 
         tk.Button(self, text="Jogar", font=("Arial Black", 20),
-                  command=ir_para_jogo).pack(pady=10)
+                  command=ir_para_jogo, bg="white", fg="black").pack(pady=10)
         
         tk.Button(self, text="Estatísticas", font=("Arial Black", 20),
-                  command=ir_para_estatisticas).pack(pady=10)
+                  command=ir_para_estatisticas, bg="white", fg="black").pack(pady=10)
 
         tk.Button(self, text="Sair", font=("Arial Black", 20),
-                  command=master.destroy).pack(pady=10)
+                  command=master.destroy, bg="white", fg="black").pack(pady=10)
